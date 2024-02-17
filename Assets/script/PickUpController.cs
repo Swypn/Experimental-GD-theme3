da8e1b10dc5189f81be2ce0792f7d4af9957ed77
swypn <willy.pe00@gmail.com>
2024-02-18 00:03:38 +0100

    I have added the script PickUpController that focus on the pick up mechanic and the throw and drop

---
+++ Assets/script/PickUpController.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    public Transform grapPosition;
+
+    public float minThrowForce = 5f;
+    public float maxThrowForce = 20f;
+    public float chargeSpeed = 10f;
+
+    private GameObject heldObject = null;
+    private float currentThrowForce;
+    private bool isCharging = false;
+
+    void Update()
+    {
+        HandleInput();
+
+        if (isCharging && heldObject != null)
+        {
+            currentThrowForce += chargeSpeed * Time.deltaTime;
+            currentThrowForce = Mathf.Min(currentThrowForce, maxThrowForce);
+        }
+    }
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObject == null)
+            {
+                TryPickupObject();
+            }
+            else
+            {
+                DropObject();
+            }
+        }
+
+        // Start charging when Q is pressed
+        if (Input.GetKeyDown(KeyCode.Q) && heldObject != null)
+        {
+            isCharging = true;
+            currentThrowForce = minThrowForce;
+        }
+
+        // Throw the object when Q is released
+        if (Input.GetKeyUp(KeyCode.Q) && isCharging)
+        {
+            isCharging = false;
+            ThrowObject();
+        }
+    }
+
+    private void TryPickupObject()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward * 2f, Color.red, 2f);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
+        {
+            Debug.Log("Raycast hit: " + hit.collider.name); // To check what the raycast hits
+            if (hit.collider.gameObject.CompareTag("Grabbable"))
+            {
+                heldObject = hit.collider.gameObject;
+                heldObject.GetComponent<Rigidbody>().isKinematic = true; // Disable physics
+                heldObject.transform.position = grapPosition.position; // Move to grab point
+                heldObject.transform.parent = grapPosition; // Parent to the grab point
+                Debug.Log("Picked up: " + hit.collider.name);
+            }
+        }
+    }
+
+    private void ThrowObject()
+    {
+        heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        heldObject.transform.parent = null;
+        heldObject.GetComponent<Rigidbody>().AddForce(transform.forward * currentThrowForce, ForceMode.VelocityChange);
+        heldObject = null;
+        currentThrowForce = 0;
+    }
+
+    private void DropObject()
+    {
+        heldObject.GetComponent<Rigidbody>().isKinematic = false;
+        heldObject.transform.parent = null;
+        heldObject = null;
+    }
+}

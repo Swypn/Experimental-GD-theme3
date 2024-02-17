99ead169e27cab1cebd5e4485cc0beb309e7daa4
swypn <willy.pe00@gmail.com>
2024-02-17 23:59:31 +0100

    I have made the code for playerController for movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public CharacterController characterController;
+    public float speed = 5.0f;
+    public float mouseSensitivity = 100.0f;
+    public Transform playerCamera;
+    public float gravity = -9.81f;
+
+    private float cameraPitch = 0.0f;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        groundedPlayer = characterController.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        characterController.Move(move * Time.deltaTime * speed);
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        characterController.Move(playerVelocity * Time.deltaTime);
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        cameraPitch -= mouseY;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraPitch;
     }
 }
